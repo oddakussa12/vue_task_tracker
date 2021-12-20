d9faaa06d2468f46344b1a0a8b2c670631d8aa42
Odda Kussa <oddakussa@outlook.com>
2021-12-20 21:05:59 +0300

    progressed 50% of the task tracker app

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,100 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+<div class="container">
+  <Header title="Task Tracker"/>
+  <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks"/>
+</div>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-  components: {
+  import Header from "./components/Header.vue"
+  import Tasks from "./components/Tasks.vue"
+  export default {
+    name: 'App',
+    components: {
+      Header,
+      Tasks,
+    },
+    data(){
+      return{
+        tasks:[]
+      }
+    },
+    methods:{
+      deleteTask(id){
+        // console.log('task',id);
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+      },
+      toggleReminder(id){
+        this.tasks = this.tasks.map((task) => 
+        task.id === id ? {...task, reminder: !task.reminder} :task)
+      }
+    },
+    created(){
+      this.tasks = [
+        {
+          id:1,
+          text:"text one",
+          day:"day 1",
+          reminder:true
+        },
+        {
+          id:2,
+          text:"text two",
+          day:"day 1",
+          reminder:true
+        },
+        {
+          id:3,
+          text:"text three",
+          day:"day 1",
+          reminder:false
+        },
+      ]
+    },
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* global styles */
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
 }
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button @click="addTask()" :style="{background:color}" >{{text}}</button>
+</template>
+
+<script>
+    export default{
+        name:"Button",
+        props:{
+            text:String,
+            color:String
+        },
+        methods:{
+            addTask(){
+                console.log("clicked");
+            }
+        }
+    }
+</script>
